b062d0e80e4798ae52e710945275e703570fca53
Omer Degirmenci <omer.degirmenci@system4.nl>
2022-11-25 21:58:13 +0100

    commit

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,18 @@
-import Link from 'next/link'
-import React from 'react'
-import { title } from '../styles/Header.module.css'
+import Link from "next/link";
+import React from "react";
+import { title } from "../styles/Header.module.css";
 // import headerStyles from '../styles/Header.module.css'
 
 const Header = () => {
-  let x = 2
   return (
     <header>
-      <div className='container'>
-        <Link passHref href='/'>
-          <h2>Dev Blog</h2>
+      <div className="container">
+        <Link passHref href="/">
+          <h2>Rumi&apos;s Keuken</h2>
         </Link>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
